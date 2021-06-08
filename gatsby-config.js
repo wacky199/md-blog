ce867b1f63f6e82c550b4a1f603b6e351870ffed
wacky <shivamsingh01111999@gmail.com>
2021-06-08 17:17:00 +0530

    added other posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `postd`,
+        name: `posts`,
         path: `${__dirname}/src/posts`,
       },
     },
