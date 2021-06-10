11f85c8620de77f5fca60d95c2f341e11deb67b1
wacky <shivamsingh01111999@gmail.com>
2021-06-10 16:26:39 +0530

    "created page for each blog"

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,14 +1,42 @@
-const path = require("path")
+const path = require(`path`)
 
 module.exports.onCreateNode = ({ node, actions }) => {
   const { createNodeField } = actions
-  if (node.internal.type === "MarkdownRemark") {
-    const slug = path.basename(node.fileAbsolutePath, ".md")
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = path.basename(node.fileAbsolutePath, `.md`)
 
     createNodeField({
       node,
-      name: "slug",
+      name: `slug`,
       value: slug,
     })
   }
+}
+
+module.exports.createPages=async ({graphql,actions}) => {
+    const {createPage}=actions
+    const blogTemplate=path.resolve(`./src/templates/blog.js`)
+    const res= await graphql(`
+      query{
+        allMarkdownRemark {
+          edges {
+            node {
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `)
+
+    res.data.allMarkdownRemark.edges.forEach((edge)=>{
+        createPage({
+            component:blogTemplate,
+            path:`/blog/${edge.node.fields.slug}`,
+            context:{
+                slug:edge.node.fields.slug,
+            }
+        })
+    })
 }
---
+++ src/templates/blog.js
@@ -0,0 +1,7 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const Blog = () => {
+  return <Layout>This is Blog Template</Layout>
+}
+export default Blog
